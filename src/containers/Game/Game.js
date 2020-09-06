cda6e0ca1cf15ff38717d1899ae8be131addf30d
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-09-06 19:03:48 +0300

    First page

---
+++ src/containers/Game/Game.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './Game.css'
+
+const Game = props => {
+
+    return (
+        <div className={classes.Game}>
+            Game page
+        </div>
+    )
+}
+
+export default Game
