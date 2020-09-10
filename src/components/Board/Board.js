ae98840861549d891c2614ee4c447a77750bdb3f
Vladislav Podkaura <tripledoublehs@ukr.net>
2020-09-10 20:24:33 +0300

    Tile component added

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import classes from './Board.module.scss'
+import Tile from '../../components/Tile/Tile'
 
 const Board = props => {
 
     return (
         <div className={classes.Board}>
-            
+            <Tile />
         </div>
     )
 }
---
+++ src/components/Tile/Tile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './Tile.module.scss'
+
+const Tile = props => {
+
+    return (
+        <div className={classes.Tile}>
+            
+        </div>
+    )
+}
+
+export default Tile
